d0d36b16626d4528e873ac66ac2837da4b5e5671
Scott Taylor <s.taylor06@gmail.com>
2020-01-27 10:32:27 -0700

    removing the new function for the do_shortcode...

--- wp-content/themes/one-confluence-version-4/functions.php
+++ wp-content/themes/one-confluence-version-4/functions.php
@@ -142,21 +142,6 @@ if (!isset($content_width))
  *
  */
 
-function oneConfluence_get_wysiwyg_output( $meta_key, $post_id = 0 ) {
-	global $wp_embed;
-
-	$post_id = $post_id ? $post_id : get_the_id();
-
-	$content = get_post_meta( $post_id, $meta_key, 1 );
-	$content = $wp_embed->autoembed( $content );
-	$content = $wp_embed->run_shortcode( $content );
-	$content = wpautop( $content );
-	$content = do_shortcode( $content );
-
-	return $content;
-}
-
-
 //Creating Custom Post types for Team
 function setup_team_cpt(){
     $labels = array(
