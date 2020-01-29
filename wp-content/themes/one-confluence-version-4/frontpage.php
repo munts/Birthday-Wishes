709f37406d1127c5bbc3058ba333de375688ceb6
Scott Taylor <s.taylor06@gmail.com>
2020-01-29 13:28:49 -0700

    cleanup work around the team/now gallery section

--- wp-content/themes/one-confluence-version-4/frontpage.php
+++ wp-content/themes/one-confluence-version-4/frontpage.php
@@ -151,46 +151,8 @@ if (function_exists('one_get_option')) {
     </section>
 
     <?php
-    if ($teamLayout === 'fluid') { get_template_part('templates/team-fluid'); }
-    else { ?>
-        <section id="gallery" class="row" data-title="<?= $dataTitleTeam; ?>">
-            <div class="col-lg-10 col-lg-push-1 team_div" style="background-image: url('<?= $team_bg; ?>');">
-                <div class="row">
-                    <div class="col-xs-8 col-xs-push-2 col-sm-8 col-sm-push-2">
-                        <div class="owl-carousel owl-theme">
-                            <?php
-                            $team2 = get_post_meta( $team_id, 'pks_team_group', true ); // this particular instance spits out 33 which is entered into a field in wp-admin
-                            foreach( (array) $team2 as $team ){
-                                // Default all variables in case they are not defined in the dataset.
-                                $name = '';
-                                $title = '';
-                                $bio = '';
-                                $img = '';
-                                if ( isset( $team['name'] ) )
-                                    $name = esc_html( $team['name'] );
-                                if ( isset( $team['title'] ) )
-                                    $title = esc_html( $team['title'] );
-                                if ( isset( $team['bio'] ) )
-                                    $bio = esc_html( $team['bio'] );
-                                if ( isset( $team['image_id'] ) ) {
-                                    $img = wp_get_attachment_image($team['image_id'], 'share-pick', null, array(
-                                        'class' => 'thumb  img-responsive grayscale',
-                                    ));
-                                } ?>
-                                <div class="team-member">
-                                    <div class="col-xs-12 col-sm-4 col-md-5 team-photo"><?= $img; ?></div>
-                                    <div class="col-xs-12 col-sm-8 col-md-7 details">
-                                        <h3><?= $name; ?></h3>
-                                        <h4><?= $title; ?></h4>
-                                        <p><?= $bio; ?></p>
-                                    </div>
-                                </div>
-                            <?php } // END foreach ?>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section> <?php } ?>
+    get_template_part('templates/team-fluid');
+   ?>
 
 
 
